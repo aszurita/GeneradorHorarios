aa06cfce5993572009fab64b8621ce17bc9213fb
aszurita <nzuritag@fiec.espol.edu.ec>
2024-12-21 09:29:09 -0500

    Resolver merge e implementación de componentes en la app

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,36 @@
-import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Miprimercomponente from "./components/Componente";
-import Calendar from "./components/HorarioSemanal";
-
+import "./App.css"
+import Calendar from "./components/HorarioSemanal"
+import ControladorListaDesplegable from "./components/ControladorListaDesplegable"
 export default function App() {
   return (
-    <>
-      <header>
-        <div className='head'>
-          <p className='schedule-maker'>Schedule Maker</p>
-          <img className='logo-espol' src='https://media.discordapp.net/attachments/1305703712903397377/1319877981979545682/Espol_Logo_2023_pa.png?ex=67678fa9&is=67663e29&hm=8202d0c6193efb292fdaa64fabe061868833f3a14b6b8e960207fee3ff3f2ce2&=&format=webp&quality=lossless' />
-          <img className='logo-taws' src='https://media.discordapp.net/attachments/1305703712903397377/1319889113658429553/logoTawsBig-DODSMUZz.png?ex=67679a07&is=67664887&hm=c0583bc290442da4547c9469e674aca9721ebf0e1ac93c10d87c726d1ba7731b&=&format=webp&quality=lossless&width=280&height=350'/>
-          <div className='linea-amarilla'></div>
-          <div className='bloque-azul'></div>
+    <div className="flex flex-col">
+      <div className="flex flex-col w-full">
+        <div className="flex items-center justify-between h-16 bg-gray-100 relative">
+          <div className="flex items-center">
+            <img
+              className="w-[125px] h-[45px] opacity-50 ml-4"
+              src="https://media.discordapp.net/attachments/1305703712903397377/1319877981979545682/Espol_Logo_2023_pa.png?ex=67678fa9&is=67663e29&hm=8202d0c6193efb292fdaa64fabe061868833f3a14b6b8e960207fee3ff3f2ce2&=&format=webp&quality=lossless"
+              alt="Logo Espol"
+            />
+            <p className="text-[#787878] text-lg mt-3 font-sans">Schedule Maker</p>
+          </div>
+          <img
+            className="w-[60px] h-[60px] mr-4"
+            src="https://media.discordapp.net/attachments/1305703712903397377/1319889113658429553/logoTawsBig-DODSMUZz.png?ex=67679a07&is=67664887&hm=c0583bc290442da4547c9469e674aca9721ebf0e1ac93c10d87c726d1ba7731b&=&format=webp&quality=lossless&width=280&height=350"
+            alt="Logo Taws"
+          />
         </div>
-      </header>
-      <h1 className="text-3xl font-bold">GENERADOR HORARIOS</h1>
-      <Miprimercomponente></Miprimercomponente>
-      <h1>Scheduler Example</h1>
-      <Calendar />
-    </>
-  );
+        <div className="w-full h-[4px] bg-[#FAB900]"></div>
+        <div className="w-full h-[26px] bg-[#001C43]"></div>
+      </div>
+      <div className="flex w-full place-content-center">
+        <div className="w-4/12">
+          <ControladorListaDesplegable />
+        </div>
+        <div className="w-8/12">
+          <Calendar />
+        </div>
+      </div>
+    </div>
+  )
 }
